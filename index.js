0b42a017d12fbfdf52445f0bd019b8a807e8ec80
pineapple <longchao.zhang@b-and-qchina.com>
2019-03-20 22:37:41 +0800

    add vue-router

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './src/App.vue'
+import router from './src/router/index'
 
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    router
 })
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
         // contentBase: path.join(__dirname,'dist'),加了HtmlWebpackPlugin后好像这个并没有什么卵用了
         port: 9999,
         host: '0.0.0.0',
+        hot: true
         // index: 'index.html'
     },
     module: {
@@ -31,7 +32,17 @@ module.exports = {
             {
                 test: /\.vue$/,
                 use: ['vue-loader']
-            }
+            },
+            {
+				test: /\.(jpg|gif|png|jpeg|svg|bmp)$/,
+				use: [{
+					loader: "url-loader",
+					options: {
+						//如果图片超过下面这个数据所标注的大小，那么图片将不会被转换成base64的格式，直接会将图片文件扔到dist目录里
+						limit: 1024 * 50
+					}
+				}]
+			},
         ],
        
     },
