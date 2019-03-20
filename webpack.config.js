5c19e95d0f9250693261807b331704c9c5aba79d
pineapple <longchao.zhang@b-and-qchina.com>
2019-03-20 16:40:49 +0800

    init and add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+    mode: 'development',
+
+    entry: path.join(__dirname,'src','index.js'),
+    output: {
+        path: path.join(__dirname,'dist'),
+        filename: 'bundle.js'
+    }
+}
